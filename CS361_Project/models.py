8b9772b28dd180f652da340d2bfb4cda14911f96
YuriKleyman <40747797+YuriKleyman@users.noreply.github.com>
2023-11-24 15:41:49 -0600

    fixup! Created Supervisor Table and fixed Account primary key

--- CS361_Project/models.py
+++ CS361_Project/models.py
@@ -5,11 +5,13 @@ class Account(models.Model):
     account_id = models.AutoField(primary_key=True)
     username = models.CharField(max_length=30, unique=True)
     password = models.CharField(max_length=30)
+    '''
     phone = models.CharField(max_length=15)
     email = models.EmailField(max_length=254)
     address = models.CharField(max_length=255)
     office_hour_location = models.CharField(max_length=255)
     office_hour_time = models.CharField(max_length=50)
+    '''
     role = models.IntegerField(choices=[(0, "Supervisor"), (1, "Instructor"), (2, "TA")])
 
     def __str__(self):
@@ -18,7 +20,7 @@ class Account(models.Model):
 
 class Supervisor(models.Model):
     supervisor_id = models.OneToOneField(Account, on_delete=models.CASCADE, primary_key=True)
-    # TODO Supervisor database
+
 
 
 class Instructor(models.Model):
