e2c9b1a69a0e348456220c2a13e7e4cd566f7c60
YuriKleyman <40747797+YuriKleyman@users.noreply.github.com>
2023-11-30 22:20:13 -0600

    reorganized functions

--- CS361_Project/funcs_for_views.py
+++ CS361_Project/funcs_for_views.py
@@ -17,3 +17,31 @@ def create_account(request):
     address=formAddress
   )
   newAccount.save()
+
+  # Helper Method for EditAccount POST
+def updateAccount(self, request, selected_account):
+    # Update user information with form data
+    selected_account.username = request.POST['username']
+    selected_account.password = request.POST['password']
+    selected_account.role = request.POST['role']
+    selected_account.name = request.POST['name']
+    selected_account.phone = request.POST['phone']
+    selected_account.email = request.POST['email']
+    selected_account.address = request.POST['address']
+    selected_account.office_hour_location = request.POST['office_hour_location']
+    selected_account.office_hour_time = request.POST['office_hour_time']
+
+    # Save the changes
+    selected_account.save()
+
+def editProfileData(self, request, user, field_name, field_type, error_name):
+    if request.POST.get(field_name) != "":
+        new_data = request.POST.get(field_name)
+        if not isinstance(new_data, field_type):
+            raise TypeError(f"{error_name} not {field_type.__name__} fails to raise TypeError")
+
+        if new_data == "Null":
+            raise ValueError("Null value fails raise ValueError")
+
+        setattr(user, field_name.lower(), new_data)
+        user.save()
